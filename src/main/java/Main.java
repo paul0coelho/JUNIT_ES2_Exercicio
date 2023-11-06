10aebfb374e8de6376a7b888b57d94553efac33b
paul0coelho <paulosmcoelho04@gmail.com>
2023-11-06 18:12:51 +0000

    commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        BinString bin = new BinString();
+
+        System.out.println(bin.convert("Paulo"));
+        System.out.println(bin.binarise(6));
+        System.out.println(bin.sum("Paulo"));
+    }
+}
